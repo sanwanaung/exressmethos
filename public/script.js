171cea475ae1bf835a896e22ab0e53dafac87a81
San Wan Aung <sanwanaung400042@gmail.com>
2023-02-19 23:58:05 +0630

    first commit

---
+++ public/script.js
@@ -0,0 +1,139 @@
+// const url = "http://localhost:3000/users";
+
+// const createUser = async () => {
+//   const newUser = {
+//     name: "Jenny's Fav Boi",
+//     email: "jennyfavboi@gmail.com",
+//     age: 24,
+//   };
+//   const response = await fetch(url, {
+//     method: "POST",
+//     headers: { "Content-Type": "application/json" },
+//     body: JSON.stringify(newUser),
+//   });
+// };
+
+// createUser();
+
+// const fetchUsers = async () => {
+//   const response = await fetch(url);
+//   const data = await response.json();
+//   console.log(data);
+// };
+
+// fetchUsers();
+
+// Trying Again
+
+const url = "http://localhost:3000/users";
+const app = document.querySelector(".app");
+const regitsteBtn = document.querySelector(".register-btn");
+
+// Updating User Interface
+const updateUser = (userInfo) => {
+  const usersDiv = document.createElement("div");
+  usersDiv.classList.toggle("usersDiv");
+  userInfo.forEach((user) => {
+    const userDiv = document.createElement("div");
+    userDiv.classList.toggle("userDiv");
+    userDiv.innerHTML += `
+      <div class="btn-container">
+        <div class="user-name">${user.name}</div>
+        <div class="btn-box">
+        <button
+          type="button"
+          class="btn btn-primary"
+          data-bs-toggle="modal"
+          data-bs-target="#exampleModal"
+          data-bs-whatever="@mdo"
+          id="${user.id}"
+          onclick="updateBtn(event)"
+        >
+          updateUserInfo
+        </button>
+
+        <button type="button" class="btn btn-danger" id="${user.id}" onclick="deleteUser(event)">delete</button>
+        </div>
+      </div>
+    `;
+    usersDiv.append(userDiv);
+  });
+  app.append(usersDiv);
+};
+
+// Register User
+regitsteBtn.addEventListener("click", async () => {
+  const userName = document.querySelector("#name").value;
+  const userEmail = document.querySelector("#email").value;
+  const userAge = document.querySelector("#age").value;
+  if (!userName && !userEmail && !userAge) {
+    return;
+  }
+  const newUser = { name: userName, email: userEmail, age: userAge };
+  const response = await fetch(url, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(newUser),
+  });
+  const data = await response.json();
+  document.querySelector("#name").value = "";
+  document.querySelector("#email").value = "";
+  document.querySelector("#age").value = "";
+  app.innerHTML = "";
+  updateUser(data);
+});
+
+// Deleting User
+const deleteUser = async (event) => {
+  const userId = event.target.id;
+  const newUserId = { id: userId };
+  const response = await fetch(url, {
+    method: "DELETE",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(newUserId),
+  });
+  const data = await response.json();
+  app.innerHTML = "";
+  updateUser(data);
+};
+
+// Update Btn
+let count;
+const updateBtn = (event) => {
+  count = event.target.id;
+};
+
+// Updating UserInfo
+const updateUserInfo = async (event) => {
+  const userName = document.querySelector(".userName").value;
+  const userEmail = document.querySelector(".userEmail").value;
+  const userAge = document.querySelector(".userAge").value;
+  if (!userName && !userEmail && !userAge) {
+    return;
+  }
+  const newUserId = {
+    name: userName,
+    email: userEmail,
+    age: userAge,
+    id: Number.parseInt(count),
+  };
+  const response = await fetch(url, {
+    method: "PUT",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(newUserId),
+  });
+  const data = await response.json();
+  document.querySelector(".userName").value = "";
+  document.querySelector(".userEmail").value = "";
+  document.querySelector(".userAge").value = "";
+  app.innerHTML = "";
+  updateUser(data);
+};
+
+// Data Fetchin From Server
+const fetchUser = async () => {
+  const response = await fetch(url);
+  const data = await response.json();
+  updateUser(data);
+};
+fetchUser();
---
+++ server.js
@@ -0,0 +1,86 @@
+// const express = require("express");
+// const app = express();
+// const port = 3000;
+
+// app.use(express.static("public"));
+// app.use(express.json());
+
+// const users = [
+//   { name: "Rose's Fav Boi", email: "rosefavboi@gmail.com", age: 22 },
+//   { name: "Lisa's Fav Boi", email: "lisafavboi@gmail.com", age: 23 },
+// ];
+
+// app.get("/users", (req, res) => {
+//   res.send(users);
+// });
+
+// app.post("/users", (req, res) => {
+//   console.log(req.body);
+//   const newUser = req.body;
+//   users.push(newUser);
+//   res.send(users);
+// });
+
+// app.listen(port, () => {
+//   console.log(`Server Started: Listenning on port ${port}`);
+// });
+
+// Trying Again
+
+const express = require("express");
+const app = express();
+const port = 3000;
+
+let users = [
+  { name: "Rose's Fav Boi", email: "rosefavboi", age: 22, id: 1 },
+  { name: "Jenny's Fav Boi", email: "jennyfavboi", age: 23, id: 2 },
+];
+
+app.use(express.static("public"));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("hi");
+});
+
+app.get("/users", (req, res) => {
+  res.send(users);
+});
+
+app.post("/users", (req, res) => {
+  // console.log(req.body);
+  const newUser = req.body;
+  newUser.id = users.length + 1;
+  users.push(newUser);
+  res.send(users);
+});
+
+app.delete("/users", (req, res) => {
+  const userId = req.body;
+  const newUserId = userId.id;
+  const filterUser = users.filter((user) => {
+    return user.id !== Number.parseInt(newUserId);
+  });
+  users = filterUser;
+  console.log(users);
+  res.send(users);
+});
+
+app.put("/users", (req, res) => {
+  const userId = req.body;
+  console.log(userId);
+  const newUserId = userId.id;
+  const checkUserId = users.find((user) => {
+    return user.id === newUserId;
+  });
+  if (checkUserId) {
+    checkUserId.name = userId.name;
+    checkUserId.email = userId.email;
+    checkUserId.age = Number.parseInt(userId.age);
+    res.send(users);
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server Started: Listenning on port ${port}`);
+});
